194ce561146c193ee5d58456e1ebaaedd5ef5931
drewwilenzick <140657499+drewwilenzick@users.noreply.github.com>
2023-07-26 16:56:28 -0400

    Update run_rosch1975_alternatePOPLM.sh

--- typicality_experiments/experiments/run_rosch1975_alternatePOPLM.sh
+++ typicality_experiments/experiments/run_rosch1975_alternatePOPLM.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
+# MAKE batchsize the length of the file -- currently batching is not supported so it is required to use one batch
 
 declare -a mlmmodels=("bert-large-uncased" "roberta-base" "roberta-large" "distilbert-base-uncased")
 declare -a incrementalmodels=("openai-gpt" "gpt2" "gpt2-medium" "distilgpt2")
 
-# # echo "Running recreation experiments on Masked Language Models"
+echo "Running recreation experiments on Masked Language Models"
 
-# # for model in ${mlmmodels[@]}
-# # do
-# #     echo "Running experiments for ${model}!"
-# #     python3 ../python/taxonomic_verification_recreat.py --model ${model} --device cpu --batchsize 565 --dataset ../data/rosch1975/rosch1975_alternate.csv
-# # done
+for model in ${mlmmodels[@]}
+do
+    echo "Running experiments for ${model}!"
+    python3 ../python/taxonomic_verification_recreate.py --model ${model} --device cpu --batchsize 565 --dataset ../data/rosch1975/rosch1975_alternate.csv
+done
 
-# echo "Running recreation experiments on Incremental Language Models"
+echo "Running recreation experiments on Incremental Language Models"
 
-# for model in ${incrementalmodels[@]}
-# do
-#     echo "Running experiments for ${model}!"
-#     python3 ../python/taxonomic_verification_recreat.py --model ${model} --device cpu --lmtype incremental --batchsize 565 --dataset ../data/rosch1975/rosch1975_alternate.csv
-# done
+for model in ${incrementalmodels[@]}
+do
+    echo "Running experiments for ${model}!"
+    python3 ../python/taxonomic_verification_recreate.py --model ${model} --device cpu --lmtype incremental --batchsize 565 --dataset ../data/rosch1975/rosch1975_alternate.csv
+done
 
 
 echo "Running POPLM experiments on Masked Language Models"
